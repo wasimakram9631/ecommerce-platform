e1b90a118c9f436e63f1c111fb5c503e1a4f5317
wasimakram9631 <129883650+wasimakram9631@users.noreply.github.com>
2024-07-03 12:33:26 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmpIG3y0QTFW005WdCoYyd-PbJPt4wGkM",
+  authDomain: "ecommerce-65d80.firebaseapp.com",
+  projectId:  "ecommerce-65d80",
+  storageBucket: "ecommerce-65d80.appspot.com",
+  messagingSenderId: "588391730358",
+  appId: "1:588391730358:web:8edf620eb81f9923592a17",
+  measurementId: "G-LRWY05NWFJ"
+};
+// Initialize Firebase
+
+export const app = initializeApp(firebaseConfig);
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+// Initialize Firebase
